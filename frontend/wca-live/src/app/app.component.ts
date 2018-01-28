9ff62cf0ccf4b59bb03fea64fbc0f2fd5debd83d
Ronald Mansveld <ronald.mansveld@gmail.com>
2018-01-28 12:41:34 +0000

    Initial frontend setup

---
+++ frontend/wca-live/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wca-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'wca';
+}
