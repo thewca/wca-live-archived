74cb7f208b9772a6896e60ee6de58c50e3935521
Ronald Mansveld <ronald.mansveld@gmail.com>
2018-01-28 13:16:04 +0000

    Include angular material and other initial setups

--- frontend/wca-live/src/app/app.module.ts
+++ frontend/wca-live/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
