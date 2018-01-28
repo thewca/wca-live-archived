84e4622eec2b3032e54dc7bdadfc84b4af090cab
Ronald Mansveld <ronald.mansveld@gmail.com>
2018-01-28 15:57:06 +0000

    Add service worker

--- frontend/wca-live/src/app/app.module.ts
+++ frontend/wca-live/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppRoutingModule } from './app-routing.module';
-
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
     BrowserAnimationsModule,
     AppRoutingModule
   ],
