c20aef24e66fa03437b4a2e2197b967ec6cfb470
Ronald Mansveld <ronald.mansveld@gmail.com>
2018-10-04 22:14:59 +0100

    Add competitor list to admin results page

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdminRoutingModule } from './admin-routing.module';
 import { CompetitionsComponent } from './competitions/competitions.component';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,10 +7,12 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { CompetitionComponent } from './competition/competition.component'
 import { MatExpansionModule } from '@angular/material/expansion';
 import { ResultsComponent } from './results/results.component';
+import { CommonServicesModule } from '../common-services/common-services.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    CommonServicesModule,
     AdminRoutingModule,
     MatButtonModule,
     MatExpansionModule,
--- frontend/src/app/admin/results/results.component.ts
+++ frontend/src/app/admin/results/results.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { ResultService } from '../../common-services/result/result.service';
 
 @Component({
   selector: 'wca-results',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent implements OnInit {
+  public results$: Observable<any[]>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private readonly _resultService: ResultService) {
+
+    this.results$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this._resultService.getForRound(params.get('id'),params.get('roundId')))
+    );
+  }
 
   ngOnInit() {
   }
-
 }
